4f852cc4ced44ba2505d68269f0e047f99eda1dd
Eduardor9 <eduardoromeror9@outlook.com>
2021-06-29 07:10:18 -0400

    Proyecto Django, migracion de BD, formularios y categorias

--- bases/models.py
+++ bases/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ClaseModelo(models.Model):
+    estado = models.BooleanField(default=True) 
+    fc = models.DateTimeField(auto_now_add=True) #fecha creacion
+    fm = models.DateTimeField(auto_now=True) # fecha modificación
+    uc = models.ForeignKey(User, on_delete=models.CASCADE) # usuario creador
+    um = models.IntegerField(blank=True, null=True) # usuario modificado
+    
+    class Meta: #no tomar en cuenta al hacer la migracion
+        abstract = True
---
+++ inv/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.db.models.base import Model
+from django.forms import widgets
+from .models import Categoria
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ['descripcion', 'estado']
+        labels = {
+            'descripcion': 'Descripcion de la categoria',
+            'estado': 'Estado de la categoria',
+        }
+        widget = {
+            'descripcion': forms.TextInput
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs({
+                'class': 'form-control',
+            })                
