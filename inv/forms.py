b18f660fc946a6736a576aa7bb2c1ad02a02cd61
Eduardor9 <eduardoromeror9@outlook.com>
2021-06-30 05:02:52 -0400

    Proyecto Django, agregar, listar, actualizar registros

--- inv/forms.py
+++ inv/forms.py
@@ -18,6 +18,6 @@ class CategoriaForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in iter(self.fields):
-            self.fields[field].widget.attrs({
+            self.fields[field].widget.attrs.update({
                 'class': 'form-control',
             })                
--- inv/views.py
+++ inv/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from .models import Categoria
+from .forms import CategoriaForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 class CategoriaView(LoginRequiredMixin, generic.ListView):
@@ -9,3 +11,31 @@ class CategoriaView(LoginRequiredMixin, generic.ListView):
     template_name = 'inv/categoria_list.html'
     context_object_name = 'obj'
     login_url = 'bases:login'
+    
+
+class CategoriaNew(LoginRequiredMixin, generic.CreateView):
+    model = Categoria
+    template_name = 'inv/categoria_form.html'
+    context_object_name = 'obj'
+    form_class = CategoriaForm
+    success_url = reverse_lazy('inv:categoria_list')
+    login_url = 'bases:login'
+    
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        
+        return super().form_valid(form)
+    
+    
+class CategoriaEdit(LoginRequiredMixin, generic.UpdateView):
+    model = Categoria
+    template_name = 'inv/categoria_form.html'
+    context_object_name = 'obj'
+    form_class = CategoriaForm
+    success_url = reverse_lazy('inv:categoria_list')
+    login_url = 'bases:login'
+    
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        
+        return super().form_valid(form)
