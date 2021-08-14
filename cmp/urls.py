11bf5f4f06b5b1fad56fe749740bd73597493175
Eduardor9 <eduardoromeror9@outlook.com>
2021-08-14 05:50:19 -0400

    Proyecto Django, modulo para imprimir y mostrar facturas

--- cmp/urls.py
+++ cmp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
 from .views import ProveedorView, ProveedorNew, ProveedorEdit, proveedorInactivar, ComprasView, compras, CompraDetDelete
+from .reportes import reporte_compras, imprimir_compra
 
 
 urlpatterns = [
@@ -13,6 +14,7 @@ urlpatterns = [
     path('compras/new', compras, name="compras_new"),
     path('compras/edit/<int:compra_id>',compras, name="compras_edit"),
     path('compras/<int:compra_id>/delete/<int:pk>',CompraDetDelete.as_view(), name="compras_del"),
-
-
+    
+    path('compras/listado', reporte_compras, name='compras_print_all'),
+    path('compras/<int:compra_id>/imprimir', imprimir_compra,name="compras_print_one"),
 ]
