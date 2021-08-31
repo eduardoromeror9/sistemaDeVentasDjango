6612e7076d2ee5c874c1b26dac7dcd350e664f53
Eduardor9 <eduardoromeror9@outlook.com>
2021-08-31 09:07:21 -0400

    Proyecto Django, facturacion completa

--- fac/reportes.py
+++ fac/reportes.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-# from django.utils.dateparse import parse_date
-# from datetime import timedelta
+from django.utils.dateparse import parse_date
+from datetime import timedelta
 
 from .models import FacturaEnc,FacturaDet
 
 def imprimir_factura_recibo(request,id):
-    template_name="fac/factura_one.html"
+    template_name = "fac/factura_one.html"
 
     enc = FacturaEnc.objects.get(id=id)
     det = FacturaDet.objects.filter(factura=id)
@@ -18,21 +18,21 @@ def imprimir_factura_recibo(request,id):
 
     return render(request,template_name,context)
 
-# def imprimir_factura_list(request,f1,f2):
-#     template_name="fac/facturas_print_all.html"
+def imprimir_factura_list(request,f1,f2):    
+    template_name = "fac/facturas_print_all.html"
 
-#     f1=parse_date(f1)
-#     f2=parse_date(f2)
-#     f2=f2 + timedelta(days=1)
+    f1 = parse_date(f1)
+    f2 = parse_date(f2)
+    f2 = f2 + timedelta(days=1)
 
-#     enc = FacturaEnc.objects.filter(fecha__gte=f1,fecha__lt=f2)
-#     f2=f2 - timedelta(days=1)
+    enc = FacturaEnc.objects.filter(fecha__gte=f1,fecha__lt=f2)
+    f2 = f2 - timedelta(days=1)
     
-#     context = {
-#         'request':request,
-#         'f1':f1,
-#         'f2':f2,
-#         'enc':enc
-#     }
-
-#     return render(request,template_name,context)
+    context = {
+        'request':request,
+        'f1':f1,
+        'f2':f2,
+        'enc':enc
+    }
+ 
+    return render(request, template_name, context)
--- fac/urls.py
+++ fac/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import ClienteView, ClienteNew, ClienteEdit, clienteInactivar, FacturaView, facturas, \
     ProductoView, borrar_detalle_factura
 
-from .reportes import imprimir_factura_recibo
+from .reportes import imprimir_factura_recibo, imprimir_factura_list
 
 urlpatterns = [
     path('clientes/',ClienteView.as_view(), name="cliente_list"),
@@ -19,5 +19,6 @@ urlpatterns = [
     path('facturas/borrar-detalle/<int:id>',borrar_detalle_factura, name="factura_borrar_detalle"),
     
     path('facturas/imprimir/<int:id>',imprimir_factura_recibo, name="factura_imprimir_one"),
-
+    
+    path('facturas/imprimir-todas/<str:f1>/<str:f2>',imprimir_factura_list, name="factura_imprimir_all"),
 ]
